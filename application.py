d119720173e3d12e09b5527d144a9687677b2689
Felipe Mendes <felipe.mendes@armazempb.com.br>
2023-07-21 11:26:45 -0300

    tir e venv

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
 import os
 
+from app.resources.commons import data_atual
+
 # executa arquivos  
 print("Entrou no arquivo application.py")
+
+print(data_atual())
 
 #os.system('python app/MATA020TESTSUITE.py')
